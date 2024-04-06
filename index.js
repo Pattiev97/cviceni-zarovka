ba320e6b457bd4a360b57c4521c9c03cfa77250f
Patricie Vyhlídalová <patina.vy@gmail.com>
2024-04-06 14:12:30 +0200

    Přidání funkce na rozsvícení

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('funguju!');
+
+const bulb = document.querySelector('.bulb');
+
+const bulbOn = () => {
+  bulb.classList.add('bulb--on');
+};
+
+bulb.addEventListener('keydown', bulbOn);
